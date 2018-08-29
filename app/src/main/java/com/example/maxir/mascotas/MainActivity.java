83940a748e30704cf2a3cd7e587008444317dc98
MaximilianoRolon <maxirolon@gmail.com>
2018-08-29 19:19:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/maxir/mascotas/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.maxir.mascotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Mascota> mascotas;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter viewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(false);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // set DataSet mascotas
+        mascotas = new ArrayList<Mascota>();
+        mascotas.add(new Mascota("Gato", 5, R.drawable.ic_launcher_background));
+        mascotas.add(new Mascota("Perro", 7, R.drawable.ic_launcher_background));
+        mascotas.add(new Mascota("Flamingo", 10, R.drawable.ic_launcher_background));
+        mascotas.add(new Mascota("Hamster", 7,R.drawable.ic_launcher_background));
+        mascotas.add(new Mascota("Mono", 2, R.drawable.ic_launcher_background));
+
+
+        // specify an adapter (see also next example)
+        viewAdapter = new MascotasAdapter(mascotas);
+        recyclerView.setAdapter(viewAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/maxir/mascotas/MascotasAdapter.java
@@ -0,0 +1,70 @@
+package com.example.maxir.mascotas;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MascotasAdapter extends RecyclerView.Adapter<MascotasAdapter.MascotasViewHolder> {
+    private ArrayList<Mascota> mascotas;
+    public View v;
+
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MascotasViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView tv_mascotas;
+        public ImageView iv_mascotas;
+        public TextView tv_mascotas_raiting;
+        public MascotasViewHolder(View v) {
+            super(v);
+            tv_mascotas = v.findViewById(R.id.tv_mascotas);
+            iv_mascotas = v.findViewById(R.id.iv_mascotas);
+            tv_mascotas_raiting = v.findViewById(R.id.tv_mascotas_raiting);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MascotasAdapter(ArrayList<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MascotasAdapter.MascotasViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.mascotas_recycler_view, parent, false);
+
+        MascotasViewHolder vh = new MascotasViewHolder(contactView);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MascotasViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.tv_mascotas.setText(mascotas.get(position).getNombre());
+        holder.iv_mascotas.setImageResource(mascotas.get(position).getFoto());
+        holder.tv_mascotas_raiting.setText(Integer.toString(mascotas.get(position).getRaiting()));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mascotas.size();
+    }
+}
